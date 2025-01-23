5601e635acf5f7c90ca9498f9a4708f6f0c154ce
Emman4321 <165729510+Emman4321@users.noreply.github.com>
2025-01-22 19:16:41 -0500

    Update randomNum.py
    More indepth

--- randomNum.py
+++ randomNum.py
@@ -2,13 +2,27 @@
 
 import random
 
-def rand():
-    print("Welcome to my random number generator!")
-
-    min_num = int(input("Enter the minimum number: "))
-    max_num = int(input("Enter the maximum number: "))   
-
-    rand_num = random.randint(min_num, max_num)
-    print("Your random number is: ", rand_num)
+def rand(min_num, max_num):
     
-rand()
+    return random.randint(min_num, max_num)
+
+def main():
+    print("Welcome to my random number generator!")
+    
+    try:
+        min_num = int(input("Enter the minimum number: "))
+        max_num = int(input("Enter the maximum number: "))
+    
+        if min_num > max_num:
+            print("Error: Minimum number must be less than or equal to the max number")
+        else:
+            rand_num = rand(min_num, max_num)
+            print("Your random number is: ", rand_num)
+    
+    except ValueError:
+        print("Invalid input! Please enter valid integers.")
+
+
+    
+if __name__ == "__main__":
+    main()
